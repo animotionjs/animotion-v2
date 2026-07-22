110e560e9473b1cee136cd85aab3217638065dec
matiadev <mattcroat.dev@gmail.com>
2026-07-22 13:50:40 +0200

    chore: remove background

--- src/lib/components/Slide.svelte
+++ src/lib/components/Slide.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section
-	class="@container flex aspect-video w-[min(100dvw,calc(100dvh*16/9))] flex-col items-center justify-center overflow-hidden bg-zinc-950 p-8 text-white"
+	class="@container flex aspect-video w-[min(100dvw,calc(100dvh*16/9))] flex-col items-center justify-center overflow-hidden p-8 text-white"
 >
 	{@render children()}
 </section>
