60fe0a001a2e1fa1a32a66c45b1fd77aefb89cf5
matiadev <mattcroat.dev@gmail.com>
2026-07-23 11:50:29 +0200

    chore: update example

--- src/lib/slides/01-intro.svelte
+++ src/lib/slides/01-intro.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
-	import { pause, scene, signal } from '#lib/timeline';
+	import { layout, parallel, pause, scene, signal } from '#lib/timeline';
 
 	const opacity = signal(0);
 	const scale = signal(0);
+	const hidden = signal(true);
 
 	scene(function* () {
-		yield* opacity.tween(1, 1);
+		yield* opacity.tween(1, 0.6);
 		yield pause();
-		yield* scale.tween(1, 1);
+		yield* parallel(
+			layout(() => hidden(false), 0.6),
+			scale.tween(1, 0.6)
+		);
 		yield pause();
 	});
 </script>
 
-<h1 style:opacity={opacity()} class="text-6xl font-bold">🪄 Animotion</h1>
-<p style:scale={scale()} class="mt-4 text-2xl text-gray-400">A presentation framework</p>
+<div class="grid place-items-center gap-16">
+	<p data-layout="title" style:opacity={opacity()} class="text-6xl font-bold">🪄 Animotion</p>
+
+	<div
+		data-layout="circle"
+		style:scale={scale()}
+		class={['h-48 w-48 rounded-full bg-amber-400', { hidden: hidden() }]}
+	></div>
+</div>
