7be941dea01b1acdec250cb5cdba11fd91d795c2
matiadev <mattcroat.dev@gmail.com>
2026-08-02 16:35:36 +0200

    feat: add example

---
+++ src/scenes/06-radius.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { code, Code, createScene } from '#lib/scene';
+
+	const SCALE = 60;
+	const MAX = 4;
+	const PAD = 20;
+	const CENTER = MAX * SCALE;
+	const TEXT_OFFSET = 32;
+
+	const scene = createScene({
+		code: `
+			const scene = createScene({
+				code: \`...\`, view: 'code', radius: 2
+			})
+				.layout(() => scene.view = 'example', 0.6)
+				.tween('radius', 3, 1.4)
+				.all((s) => {
+					s.codeSelection(code.lines(5,8), 0.4);
+					s.tween('radius', 3, 1.4);
+				});
+		`,
+		view: 'code',
+		radius: 3
+	})
+		.slideTransition({ duration: 0.4 })
+		.codeSelection(code.lines(0, 2), 0.4)
+		.codeSelection(code.lines(3), 0.4)
+		.layout(() => (scene.view = 'example'), 0.6)
+		.all((s) => {
+			s.codeSelection(code.lines(4), 0.4);
+			s.tween('radius', 4, 1.4);
+		})
+		.all((s) => {
+			s.codeSelection(code.lines(5, 8), 0.4);
+			s.tween('radius', 3, 1.4);
+		});
+</script>
+
+<div class="flex items-center gap-24">
+	<div data-layout="code">
+		<Code class="text-xl" />
+	</div>
+
+	{#if scene.view === 'example'}
+		<svg
+			data-layout="scene"
+			viewBox="{-PAD} {-PAD} {CENTER * 2 + PAD * 2} {CENTER * 2 + PAD * 2}"
+			class="w-120"
+		>
+			<circle cx={CENTER} cy={CENTER} r={scene.radius * SCALE} class="fill-red-500" />
+			<line
+				x1={CENTER}
+				y1={CENTER}
+				x2={CENTER + scene.radius * SCALE}
+				y2={CENTER}
+				stroke-width="6"
+				stroke-dasharray="10 10"
+				class="stroke-black"
+			/>
+			<text
+				x={CENTER + (scene.radius * SCALE) / 2}
+				y={CENTER + TEXT_OFFSET}
+				class="fill-black font-mono text-lg font-semibold"
+				text-anchor="middle"
+				dominant-baseline="middle"
+			>
+				r = {scene.radius.toFixed(2)}
+			</text>
+		</svg>
+	{/if}
+</div>
