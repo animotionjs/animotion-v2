977106b0d017a2ad637fe2501e97f5677a23fc64
matiadev <mattcroat.dev@gmail.com>
2026-08-03 13:10:44 +0200

    chore: update example

--- src/scenes/01-intro.svelte
+++ src/scenes/01-intro.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
-	import { createScene } from '#lib/scene';
+	import { createScene, easeInOut } from '#lib/scene';
 
-	const scene = createScene({ opacity: 0, scale: 0, hidden: true })
+	const scene = createScene({ opacity: 0, view: 'title' })
 		.slideTransition({ duration: 0.4 })
 		.tween('opacity', 1, 0.6)
-		.all((s) => {
-			s.layout(() => (s.hidden = false), 0.6);
-			s.tween('scale', 1, 0.6);
-		});
+		.layout(() => (scene.view = 'circle'), 0.6, easeInOut, { enter: 'scale' });
 </script>
 
 <div class="grid place-items-center gap-16">
 	<p data-layout="title" class="text-6xl font-bold" style:opacity={scene.opacity}>🪄 Animotion</p>
 
-	<div
-		data-layout="circle"
-		class={['h-48 w-48 rounded-full bg-amber-400', { hidden: scene.hidden }]}
-		style:scale={scene.scale}
-	></div>
+	{#if scene.view === 'circle'}
+		<div data-layout="circle" class="h-48 w-48 rounded-full bg-amber-400"></div>
+	{/if}
 </div>
