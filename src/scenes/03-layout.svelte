8652950ded637ac34b3839c503f0062d17f48877
matiadev <mattcroat.dev@gmail.com>
2026-08-05 19:16:21 +0200

    fix: don't reassign state proxy

--- src/scenes/03-layout.svelte
+++ src/scenes/03-layout.svelte
@@ -2,8 +2,8 @@
 	import { createScene } from '#lib/scene';
 
 	const scene = createScene({ order: ['Svelte 5', 'SvelteKit', 'Tailwind 4', 'TypeScript'] })
-		.layout(() => (scene.order = [...scene.order].reverse()), 0.5)
-		.layout(() => (scene.order = [...scene.order].reverse()), 0.5);
+		.layout(() => scene.order.reverse(), 0.5)
+		.layout(() => scene.order.reverse(), 0.5);
 </script>
 
 <ul>
