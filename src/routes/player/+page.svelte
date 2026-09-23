80d9cedc2a82ff63929ddf0ae0e3aacb45f66b24
matiadev <mattcroat.dev@gmail.com>
2026-09-23 19:03:42 +0200

    chore: move player route to dedicated branch

---
+++ src/routes/player/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Player } from '#lib';
+	import Intro from '../../scenes/01-intro.svelte';
+</script>
+
+<div class="h-dvh w-dvw bg-black p-4">
+	<div class="mx-auto max-w-4xl">
+		<Player scene={Intro} />
+	</div>
+</div>
